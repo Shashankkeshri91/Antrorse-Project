42144c3ff60183a4983207617a7760b277a75a93
Shashank keshri <99127554+Shashankkeshri91@users.noreply.github.com>
2023-11-29 23:20:50 +0530

    Project2

---
+++ Project2/index.js
@@ -0,0 +1,32 @@
+function red() {
+    document.getElementById("content").style.backgroundColor = "red";
+  }
+  function cyan() {
+    document.getElementById("content").style.backgroundColor = "cyan";
+  }
+  function orang() {
+    document.getElementById("content").style.backgroundColor = "orange";
+  }
+  function blac() {
+    document.getElementById("content").style.backgroundColor = "black";
+  }
+  function purp() {
+    document.getElementById("content").style.backgroundColor = "purple";
+  }
+  function green() {
+    document.getElementById("content").style.backgroundColor = "green";
+  }
+  function brwn() {
+    document.getElementById("content").style.backgroundColor = "brown";
+  }
+  function blu() {
+    document.getElementById("content").style.backgroundColor = "blue";
+  }
+  function ylw() {
+    document.getElementById("content").style.backgroundColor = "yellow";
+  }
+  function magn() {
+    document.getElementById("content").style.backgroundColor = "#FF00FF";
+  }
+  
+  
