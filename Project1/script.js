c345a80be0d215fe1d9865b6623e77a50b4c7e82
Shashank keshri <99127554+Shashankkeshri91@users.noreply.github.com>
2023-12-03 10:36:16 +0530

    Project1-Update

---
+++ Project1/script.js
@@ -0,0 +1,31 @@
+function myFuncOperation(op){
+    let inputNum = parseFloat(document.getElementById("userInput").value);
+    let res = 0;
+  
+    switch(op){
+      case '+2':
+  res = inputNum + 2;
+        break;
+      case '-2':
+        res = inputNum- 2;
+        break;
+      case '*2':
+        res = inputNum * 2;
+        break;
+      case '/2':
+        res = inputNum/ 2;
+        break;
+      case '%2':
+        res = inputNum%2;
+        break;
+      default:
+        console.log("Invalid Operation");
+        return;
+    }
+    // Log the result to the console
+    console.log('Result:', res);
+  
+    // Display the result in the result box
+    document.getElementById('result').textContent = 'Result: ' + res;
+  
+  }
